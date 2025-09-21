8b41ede4ff1e347eb8a090b07b751999f416f972
Reeshaaaa <fousekrichard@gmail.com>
2025-09-21 22:18:55 +0200

    Optimized code

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-from random import randrange, choice
+from random import choice
 from characters import chars
 from user_input import UserInput
 
 user_input = UserInput()
 length = user_input.length_input()
 chosen_chars = user_input.char_select()
-password = []
-output = ""
 
-for character in range(length):
-    #appends random characters
-    char_list = choice(chosen_chars)
-    password.append(chars[char_list][randrange(0, len(chars[char_list]))])
+# Flatten the list of possible characters
+available_chars = [c for group in chosen_chars for c in chars[group]]
 
-for character in password:
-    #rewrites the password into string
-    output += character
+# Generate password using random choices
+password = ''.join(choice(available_chars) for _ in range(length))
 
-print(output)
+print(password)
--- user_input.py
+++ user_input.py
@@ -11,19 +11,19 @@ class UserInput:
     def char_select(self):
         chosen_chars = []
         nums = input("Do you want to add numbers? (y/n) ")
-        if nums == "y":
+        if nums == "y" or nums == "":
             chosen_chars.append(0)
 
         lower = input("Do you want to add lowercase letters? (y/n) ")
-        if lower == "y":
+        if lower == "y" or lower == "":
             chosen_chars.append(1)
 
         upper = input("Do you want to add uppercase letters? (y/n) ")
-        if upper == "y":
+        if upper == "y" or upper == "":
             chosen_chars.append(2)
 
         specials = input("Do you want to add special characters? (y/n) ")
-        if specials == "y":
+        if specials == "y" or specials == "":
             chosen_chars.append(3)
 
         if chosen_chars == []:
